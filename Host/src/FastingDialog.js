6babe5120d48a133cf426a0e1ed0c318bb587522
Seenu Pandiyan <seenu.sf3386@gmail.com>
2022-08-17 14:52:00 +0530

    commit remove

--- Host/src/FastingDialog.js
+++ Host/src/FastingDialog.js
@@ -7,93 +7,8 @@ function FastingDialog(props) {
 
     var diff=16;
     var [difference, setDifference] = useState({
-            diff: diff
-        })
-
-        // let circulargauge = [{
-        //     ranges: [
-        //         {
-        //             start: 0,
-        //             end: 100,
-        //             radius: '100%',
-        //             startWidth: 30,
-        //             endWidth: 30,
-        //             color: '#E1E9ED',
-        //             roundedCornerRadius: 15,
-        //         },
-        //         {
-        //             start: 0,
-        //             end: 100,
-        //             radius: '100%',
-        //             startWidth: 30,
-        //             endWidth: 30,
-        //             color: '#CDD9E0',
-        //             roundedCornerRadius: 15,
-        //             linearGradient: {
-        //                 startValue: '0%',
-        //                 endValue: '100%',
-        //                 colorStop: [
-        //                     { color: '#FB5F64', offset: '0%', opacity: 0.9 },
-        //                     { color: '#FC9662', offset: '70%', opacity: 0.9 }]
-        //             }
-        //         },
-        //         {
-        //             start: 2,
-        //             end: 98,
-        //             radius: '91%',
-        //             startWidth: 5,
-        //             endWidth: 5,
-        //             roundedCornerRadius: 2,
-        //             color: '#FFFFFF',
-        //             opacity: 0.35
-        //         },
-        //     ],
-        //     annotations: isDevice ? [{
-        //         angle: 0,
-        //         zIndex: '1',
-        //         radius: '0%'
-        //     },
-        //     {
-        //         zIndex: '1',
-        //         radius: '91%',
-        //         angle: 350,
-        //         content: '<div class="e-gauge-percent-img icon-Calories"></div>'
-        //     },
-        //     {
-        //         zIndex: '1',
-        //         radius: '91%',
-        //         angle: 60,
-        //         content: '<div class="e-gauge-status-img icon-Diet"></div>'
-        //     },
-        //     {
-        //         zIndex: '1',
-        //         radius: '91%',
-        //         angle: 280,
-        //         content: '<div class="e-gauge-status-img icon-Thunder"></div>'
-        //     }] : [{
-        //         angle: 0,
-        //         zIndex: '1',
-        //         radius: '0%'
-        //     },
-        //     {
-        //         zIndex: '1',
-        //         radius: '90%',
-        //         angle: 350,
-        //         content: '<div class="e-gauge-percent-img icon-Calories"></div>'
-        //     },
-        //     {
-        //         zIndex: '1',
-        //         radius: '89%',
-        //         angle: 60,
-        //         content: '<div class="e-gauge-status-img icon-Diet"></div>'
-        //     },
-        //     {
-        //         zIndex: '1',
-        //         radius: '89%',
-        //         angle: 280,
-        //         content: '<div class="e-gauge-status-img icon-Thunder"></div>'
-        //     }]
-        // }];
+        diff: diff
+    })
     let dialogInstance;
     let dateStartInstance;
     let dateEndInstance;
@@ -104,25 +19,7 @@ function FastingDialog(props) {
     let target = 'body';
     let fastingDialogwidth = isDevice ? '100%' : '400px';
     let fastingDlgButtons = [{ click: props.fastingCancelBtnClick.bind(dialogInstance), buttonModel: { content: 'CANCEL', cssClass: 'e-fasting-cancel' } }, { click: props.fastingDlgBtnClick.bind(this), buttonModel: { content: 'START FASTING', cssClass: 'e-start-fast' } }];
-    let dlgPosition = { X: 'center', Y: 'center' };
-
-    // function onCreated() {
-    //     if(dateStartInstance && dateEndInstance) {
-    //         diff = Math.floor(((dateEndInstance.value) - (dateStartInstance.value)) / (1000 * 60 * 60));
-    //     }
-    //     setDifference({
-    //         [diff] : diff
-    //     })
-    // }
-
-    // function onfastChange() {
-        // diff = Math.floor(((dateEndInstance.value) - (dateStartInstance.value)) / (1000 * 60 * 60));
-        // setDifference(() => {
-        //     return {
-        //        diff : diff
-        //     }
-        // })
-    // }
+    let dlgPosition = { X: 'center', Y: 'center' }
 
     function onfastChange() {
         diff = Math.floor(((dateEndInstance.value) - (dateStartInstance.value)) / (1000 * 60 * 60));
@@ -178,5 +75,3 @@ function FastingDialog(props) {
 }
 
 export default FastingDialog;
-
-9843524454
